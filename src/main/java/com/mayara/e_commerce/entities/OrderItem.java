e3b897d21d7b1ac49131c2b9a35db5c1a77a88b2
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-02-04 11:41:34 +0000

    create many to many with join entity

---
+++ src/main/java/com/mayara/e_commerce/entities/OrderItem.java
@@ -0,0 +1,53 @@
+package com.mayara.e_commerce.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(){}
+
+    public OrderItem(Order order,Product product, Integer quantity, Double price) {
+        this.id.setOrder(order);
+        this.id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+    public void setId(Order order){
+        id.setOrder(order);
+    }
+    public Product getProduct(){
+        return id.getProduct();
+    }
+    public void setId(Product product){
+        id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
--- src/main/java/com/mayara/e_commerce/entities/Product.java
+++ src/main/java/com/mayara/e_commerce/entities/Product.java
@@ -24,6 +24,9 @@ public class Product {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product(){}
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -76,5 +79,9 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 }
