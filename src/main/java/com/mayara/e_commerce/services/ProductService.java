c4111d48b930c34884ff70d0a51abf3f8775b680
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-02-07 16:24:18 +0000

    Create FindAll and POST Product

--- src/main/java/com/mayara/e_commerce/services/ProductService.java
+++ src/main/java/com/mayara/e_commerce/services/ProductService.java
@@ -4,7 +4,10 @@ import com.mayara.e_commerce.dtos.ProductDTO;
 import com.mayara.e_commerce.entities.Product;
 import com.mayara.e_commerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
@@ -12,8 +15,28 @@ public class ProductService {
     @Autowired
     ProductRepository repository;
 
+    @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
     }
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result= repository.findAll(pageable);
+        return result.map(x->new ProductDTO(x));
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+
 }
