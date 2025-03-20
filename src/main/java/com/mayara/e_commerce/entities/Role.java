c5807a2d27e997249f139548f8d972217d71cd2d
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-03-20 15:33:30 +0000

    User Details, GrantedAuthority, not tested

--- src/main/java/com/mayara/e_commerce/entities/Role.java
+++ src/main/java/com/mayara/e_commerce/entities/Role.java
@@ -1,11 +1,12 @@
 package com.mayara.e_commerce.entities;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Objects;
 
 @Entity
 @Table(name= "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
@@ -16,7 +17,7 @@ public class Role {
         this.id = id;
         this.authority = authority;
     }
-
+    @Override
     public String getAuthority() {
         return authority;
     }
