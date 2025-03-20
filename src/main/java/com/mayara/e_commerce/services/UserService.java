4f7f05d77b60125fc7142fc77e35f0b7287435f3
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-03-20 15:37:39 +0000

    Add UserDetail with UserDetailService

---
+++ src/main/java/com/mayara/e_commerce/services/UserService.java
@@ -0,0 +1,35 @@
+package com.mayara.e_commerce.services;
+
+import com.mayara.e_commerce.entities.Role;
+import com.mayara.e_commerce.entities.User;
+import com.mayara.e_commerce.projections.UserDetailsProjection;
+import com.mayara.e_commerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+@Autowired
+    UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
+        if(result.size() == 0){
+            throw new UsernameNotFoundException("User not Found");
+        }
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result){
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+        return user;
+    }
+}
